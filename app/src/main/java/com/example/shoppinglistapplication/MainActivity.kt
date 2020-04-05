64d9413d1076ae666cc3eea6dd9e2b40ab496ac3
Anneli Klamas <annelik98@gmail.com>
2020-04-06 01:35:29 +0300

    implemented adding items to table and database

--- app/src/main/java/com/example/shoppinglistapplication/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapplication/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.shoppinglistapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,14 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loadData()
-
-
+        //loadAutoComplete()
+        val addButton = findViewById<Button>(R.id.addButton)
+        addButton.setOnClickListener { addItem() }
     }
 
     fun loadData(){
         // Access a Cloud Firestore instance from your Activity
-        val db = FirebaseFirestore.getInstance()
 
+        val db = FirebaseFirestore.getInstance()
         db.collection("shoppingList").document("jTGLEUh")
             .get()
             .addOnSuccessListener { document ->
@@ -30,27 +33,11 @@ class MainActivity : AppCompatActivity() {
                     if (data != null) {
                         val shoppingList = findViewById<TableLayout>(R.id.shoppingList)
                         for (itemInfo in data){
+                            val key = itemInfo.key
+                            val value = itemInfo.value.toString()
+                            addItemToTable(key, value, shoppingList)
 
-                            val row = TableRow(this)
-                            row.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                ViewGroup.LayoutParams.WRAP_CONTENT)
-                            val item = TextView(this)
-                            item.apply {
-                                layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
-                                    TableRow.LayoutParams.WRAP_CONTENT)
-                                val key = itemInfo.key
-                                text = key
-                            }
-                            row.addView(item)
-                            val amount = TextView(this)
-                            amount.apply {
-                                layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
-                                    TableRow.LayoutParams.WRAP_CONTENT)
-                                text = itemInfo.value.toString()
-                            }
-                            row.addView(amount)
-                            row.addView(CheckBox(this))
-                            shoppingList.addView(row)
+
 
                         }
                     }
@@ -64,5 +51,69 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    fun addItemToTable(key:String,value:String, shoppingList: TableLayout){
+        val row = TableRow(this)
+        row.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+        val item = TextView(this)
+        item.apply {
+            layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT)
 
+            text = key
+        }
+        row.addView(item)
+        val amount = TextView(this)
+        amount.apply {
+            layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT)
+            text = value
+        }
+        row.addView(amount)
+        row.addView(CheckBox(this))
+        shoppingList.addView(row)
+    }
+
+    /**fun loadAutoComplete(): List<String>{
+        val db = FirebaseFirestore.getInstance()
+        val list = listOf<String>()
+
+        db.collection("shoppingList").document("aOWMf")
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+
+
+                }
+                else {
+                    Log.d("noexist", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("errordb", "get failed with ", exception)
+            }
+        return emptyList()
+    }*/
+
+    fun addItem(){
+        var name = findViewById<EditText>(R.id.itemToAdd).text.toString()
+        var amount = findViewById<EditText>(R.id.amountToAdd).text.toString()
+        val item = hashMapOf<String, Any>(
+            name to amount
+        )
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("shoppingList").document("jTGLEUh")
+            .set(item, SetOptions.merge())
+            .addOnSuccessListener { Log.d("Added", "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w("noAdded", "Error writing document", e) }
+
+        val row = TableRow(this)
+        row.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        val shoppingList = findViewById<TableLayout>(R.id.shoppingList)
+        addItemToTable(name, amount, shoppingList)
+    }
 }
