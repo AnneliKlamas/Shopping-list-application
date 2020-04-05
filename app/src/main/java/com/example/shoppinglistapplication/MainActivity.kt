f7edff2c30b7a210e12dbc8325cfaf126dfa7d4e
Anneli Klamas <annelik98@gmail.com>
2020-04-05 21:07:42 +0300

    connected and made clode firestore database

--- app/src/main/java/com/example/shoppinglistapplication/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapplication/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.shoppinglistapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Access a Cloud Firestore instance from your Activity
+        val db = FirebaseFirestore.getInstance()
+
+        val docRef = db.collection("shoppingList").document("jTGLEUh")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d("noexist", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("errordb", "get failed with ", exception)
+            }
+
     }
 }
